4f73be07dd07b71662a3193f06eadb914b4286d9
김호준 <kimhoojun@gimhojun-ui-MacBookPro.local>
2022-04-16 09:51:12 +0800

    0416-1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Btns from './components'
+import Btns, {Card} from './components'
 
 
 function App() {
   return (
     <div className="App">
       <div className='container-fluid'>
-        <div className="row header justify-content-center">
+        <div className="row sec-header justify-content-center">
           <div className="col-3 py-3 mb-3">
             <img src="https://via.placeholder.com/100x50" className="w-img mx-auto" />
           </div>
         </div>
-        <div className="row main">
-          <div className='row row-btn'>
-            {/* <div className="col-md-3 col-lg-2 offset-lg-2">
-              <button type='button' className='btn btn-primary btn-lg'>click</button>
-            </div>
-            <div className="col-md-3 col-lg-2">
-              <button type='button' className='btn btn-primary btn-lg'>click</button>
-            </div>
-            <div className="col-md-3 col-lg-2">
-              <button type='button' className='btn btn-primary btn-lg'>click</button>
-            </div>
-            <div className="col-md-3 col-lg-2">
-              <button type='button' className='btn btn-primary btn-lg'>click</button>
-            </div> */}
-            <Btns name='letter' isFirst={true} />
-            <Btns name='album' isSecond={true} />
-            <Btns name='dboard' isThird={true} />
-            <Btns name='sentence' isFourth={true} />
+        <div className="row sec-btn">
+          <Btns name='letter' isFirst={true} />
+          <Btns name='album' isSecond={true} />
+          <Btns name='dboard' isThird={true} />
+          <Btns name='sentence' isFourth={true} />
+        </div>
+        <div className="row sec-album">
+          <div className="col-md-12 col-lg-8 offset-lg-2 row mx-auto h-100">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </div>
         </div>
       </div>
--- src/components.js
+++ src/components.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import React, {Component} from 'react';
 import letter from "./img/letter.png"
+import { Carousel } from "react-bootstrap"
 
 class Btns extends Component {
     constructor(props) {
@@ -47,4 +48,33 @@ class Btns extends Component {
     }
 }
 
-export default Btns
+function Card() {
+    return (
+        <div className='card col-lg-4 col-md-6 col-sm-12 card-item'>
+            <h3 className='card-header'>card header</h3>
+            <div className='card-body'>
+                <h5 className='title'>title</h5>
+                <h6 className="card-subtitle text-muted">Support card subtitle</h6>
+            </div>
+            {/* <div className='card-img text-center'>
+                <img src='https://via.placeholder.com/250x150' className='h-img' />
+            </div> */}
+            <Carousel variant='dark' interval={null}>
+                <Carousel.Item className='card-img text-center'>
+                    <img src="https://via.placeholder.com/100" className='h-img' />
+                </Carousel.Item>
+                <Carousel.Item className='card-img text-center'>
+                    <img src="https://via.placeholder.com/200" className='h-img' />
+                </Carousel.Item>
+                <Carousel.Item className='card-img text-center'>
+                    <img src="https://via.placeholder.com/300" className='h-img' />
+                </Carousel.Item>
+            </Carousel>
+            <div className='card-body'>
+                <p className='card-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when </p>
+            </div>
+        </div>
+    )
+}
+
+export {Btns as default, Card}
